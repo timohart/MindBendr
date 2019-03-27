73e357eba7c12220113977ed55d6a65ec6740ce4
Timo Hartwig <timothy.m.hartwig@gmail.com>
2019-03-27 15:11:48 -0500

    Initial Commit

---
+++ MindBendr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MindBendr
+//
+//  Created by Timothy Hartwig Student on 3/27/19.
+//  Copyright © 2019 CVTC Spring 2019 Capstone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
