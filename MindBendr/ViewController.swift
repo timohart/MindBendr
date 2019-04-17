673a1159a80160cf42a1c5e7ba1baf37fa2c5599
JulesVern <jklimeck@student.cvtc.edu>
2019-04-17 15:54:48 -0500

    start of logic

--- MindBendr/ViewController.swift
+++ MindBendr/ViewController.swift
@@ -19,6 +19,44 @@ class ViewController: UIViewController {
         
         
     }
+    // quiz button is pressed
+    
+    //
+    // variables
+    //
+    // var currentQuestion = 0
+    // var totalQuestions = count of total questions for that quiz
+    // var userCorrect = 0
+    
+    // quiz label (top of screen) is pulls text matching name of the button pressed
+    
+    // increment current question viariable by 1 and
+    
+    // output to currentQuestionLabel the currentQuestion of totalQuestions
+    
+    // display first question and answer options
+    
+    // foreach (currentQuestion < totalQuestion) { user selects answer
+    
+        // check if answer is true
+    
+            // change answer button to green
+    
+            // increment userCorrect by 1
+    
+        // else confirm quesiton is wrong
+    
+            // change answer button to red
+    
+        // end if else
+    
+        // timer count down to change question
+    
+        // increment currentQuestion by 1
+    
+    // } end foreach
+    
+    
     
 }
 
