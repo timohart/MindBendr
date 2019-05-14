c4d20f9f533c3082ee61678377648aca6d00ccfc
Timo Hartwig <timothy.m.hartwig@gmail.com>
2019-05-14 15:08:55 -0500

    Updated Quiz Logic

--- MindBendr/Views/QuizViewController.swift
+++ MindBendr/Views/QuizViewController.swift
@@ -15,9 +15,12 @@ class QuizViewController: UIViewController {
   // MARK: - Class Vars
     var breathingWithTimo: AVAudioPlayer?
   
-    var currentQuestion = 1
+    var currentQuestionNumber = 1
     var totalQuestions = 10
     var userCorrect = 0
+  
+  var currentQuestion : Question?
+  
   
   // timer variables
     var seconds = 0
@@ -64,8 +67,6 @@ class QuizViewController: UIViewController {
         if let path : String = Bundle.main.path(forResource: "preloadedData", ofType: "plist")! {
           questionsArray = NSArray(contentsOfFile: path)! as [AnyObject] as NSArray
         }
-      
-        runGame()
     }
 
   // MARK: - IBActions
@@ -84,54 +85,22 @@ class QuizViewController: UIViewController {
         }
 
     }
-    
-    // grab reference to plist chosen and create an array of dictionaries
-    func getPlist(withName name: String) -> [String]? {
-        if  let path = Bundle.main.path(forResource: name, ofType: "plist"),
-            let xml = FileManager.default.contents(atPath: path) {
-            return (try? PropertyListSerialization.propertyList(from: xml, options: .mutableContainersAndLeaves, format: nil)) as? [String]
-        }
-        
-        return nil
-        
-    }
   
-    //var quizQuestions = getPlist("preloadedData")
-    
-    // while current question <= totalQuesions
-    
-        // selectQuestion()
-    
-        // take randomized list of quizQuestions grab variable qText to display in questionLabel
-    
-        // grab the answers array associated with that question - could randomize as well
-        // display answerOptions on buttons for user to select from
-    
-        // if answer is true
-            // increment totalCorrect
-            // increment currentQuestion
-            // - - change button color if we have time
-        // else answer is false
-            // increment current question
-            // - - change button color if we have time
-        // end ifelse
-    
-    
-    // func selectQuestion(pass in quizQuestions)
-        // randomlize list and create an array
-        // sending to the while loop
-        // delete from the array of dictionaries.
-    // end function
+  @IBAction func AnswerButtonClick(_ sender: Any) {
+    // Check if answer is correct
+      // add to score
+      // other correct question stuff
+    // else
+      // something wrong answer
 
-    // quiz label (top of screen) is pulls text matching name of the button pressed
-    
-    // output to currentQuestionLabel the currentQuestion of totalQuestions
+    loadQuestion()
+  }
   
   // MARK: - Supporting Func
   
     func questionLabel () {
         
-    currentQuestionCounter.text = "\(currentQuestion) of \(totalQuestions)"
+    currentQuestionCounter.text = "\(currentQuestionNumber) of \(totalQuestions)"
     
     }
     
@@ -149,40 +118,31 @@ class QuizViewController: UIViewController {
                                      selector: #selector(QuizViewController.subtractTime),
                                      userInfo: nil,
                                      repeats: true)
-    }
-    
-    func runGame() {
-      var questionNumber = 1
-      while questionNumber <= 10 {
-        let selectedQuestion : [String: AnyObject] = selectCurrentQuestion()
-        
-        quizViewLabel.text = selectedQuestion["qText"] as? String
-        
-        if let answersArray = selectedQuestion["answers"] as? [AnyObject] as NSArray? {
-          var answersFilled = 0
-          
-          for answer in answersArray {
-            if let answerDic = answer as? [String: AnyObject] {
-              switch answersFilled {
-              case 0:
-                answer1Btn.setTitle(answerDic["text"] as? String, for: .normal)
-              case 1:
-                answer2Btn.setTitle(answerDic["text"] as? String, for: .normal)
-              case 2:
-                answer3Btn.setTitle((answerDic["text"] as! String), for: .normal)
-              default:
-                print("error in loading answers")
-              }
-            }
-            answersFilled = answersFilled + 1
-          }
-        }
-        questionNumber = questionNumber + 1
-      }
+      
+      loadQuestion()
     }
   
+  
+  func loadQuestion() {
+    if currentQuestionNumber <= 10 {
+      currentQuestion = selectCurrentQuestion()
+      
+      quizViewLabel.text = currentQuestion!.text
+      
+      for answer in currentQuestion!.answers {
+        // Button Load
+      }
+    } else {
+      endGame()
+    }
+  }
+  
   func endGame() {
+    // set up segue
     
+    // send score infor
+    
+    // segue
   }
     
     // subtractTime func, actually subtracts time from 30 seconds to 0
@@ -246,8 +206,10 @@ class QuizViewController: UIViewController {
   // button is pressed for breathing soundtrack
   //
   
-  func selectCurrentQuestion() -> [String: AnyObject] {
-    var selectedQuestion : [String: AnyObject]?
+  func selectCurrentQuestion() -> Question {
+    var selectedQuestion : Question?
+    var qText : String?
+    var answers : [Answer]?
     
     while (selectedQuestion == nil) {
       let selectedNum = Int.random(in: 0...questionsArray.count)
@@ -255,10 +217,23 @@ class QuizViewController: UIViewController {
         usedQuestions.append(selectedNum)
 
         if let question = questionsArray[selectedNum] as? [String:AnyObject] {
-          selectedQuestion = question
+          qText = question["qText"] as! String
+          answers = getAnswers(aDic: question["answers"] as! [[String : AnyObject]])
+          
+          selectedQuestion = Question(text: qText!, answers: answers!)
         }
       } // if
     } // While
     return selectedQuestion!
   } // func sCQ
+  
+  func getAnswers(aDic : [[String:AnyObject]]) -> [Answer] {
+    var answers: [Answer]!
+    
+    for answer in aDic {
+      answers.append(Answer(text: (answer["text"] as? String)!, isCorrect: (answer["isCorrect"] as? Bool)!))
+    }
+    
+    return answers
+  }
 }
